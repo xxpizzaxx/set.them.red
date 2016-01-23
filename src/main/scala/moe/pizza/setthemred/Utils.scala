f9b2144eab034287f0047a99a4d94a083d4d37a0
Andi Miller <andi@andimiller.net>
2016-01-23 02:35:13 +0000

    start implementing basic features

---
+++ src/main/scala/moe/pizza/setthemred/Utils.scala
@@ -0,0 +1,36 @@
+package moe.pizza.setthemred
+
+import moe.pizza.setthemred.Utils.Alerts.Alerts
+
+/**
+ * Created by Andi on 22/01/2016.
+ */
+object Utils {
+  object Alerts extends Enumeration {
+    type Alerts = Value
+    val success = Value("success")
+    val info = Value("info")
+    val warning = Value("warning")
+    val danger = Value("danger")
+  }
+  implicit class PimpedRequest(r: spark.Request) {
+    def flash(level: Alerts, message: String): Unit = {
+      val session = getSession
+      session match {
+        case Some(s) => r.session.attribute(Webapp.SESSION, s.copy(alerts = s.alerts :+ Types.Alert(level.toString, message)))
+        case None => ()
+      }
+    }
+    def getSession = Option(r.session.attribute[Types.Session](Webapp.SESSION))
+    def setSession(s: Types.Session): Unit = r.session.attribute(Webapp.SESSION, s)
+    def clearAlerts(): Unit = {
+      val session = getSession
+      session match {
+        case Some(s) => r.session.attribute(Webapp.SESSION, s.copy(alerts = List()))
+        case None => ()
+      }
+
+    }
+  }
+
+}
--- src/main/scala/moe/pizza/setthemred/Webapp.scala
+++ src/main/scala/moe/pizza/setthemred/Webapp.scala
@@ -3,13 +3,17 @@ package moe.pizza.setthemred
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.scala.DefaultScalaModule
 import moe.pizza.crestapi.CrestApi
+import moe.pizza.eveapi.generated.eve.{CharacterID, CharacterAffiliation}
 import moe.pizza.setthemred.Types.Alert
 import moe.pizza.sparkhelpers.SparkWebScalaHelpers._
-import moe.pizza.eveapi.SyncableFuture
+import moe.pizza.eveapi.{EVEAPI, SyncableFuture}
+import play.twirl.api.Html
 import spark.Spark._
 import spark._
+import Utils._
 
 import scala.concurrent.ExecutionContext.Implicits.global
+import scala.util.{Failure, Success, Try}
 
 object Webapp extends App {
 
@@ -19,6 +23,7 @@ object Webapp extends App {
   val config = OM.readValue(configtext, classOf[Config])
 
   val crest = new CrestApi(baseurl = config.login_url, cresturl = config.crest_url, config.clientID, config.secretKey, config.redirectUrl)
+  val eveapi = new EVEAPI()
   val defaultCrestScopes = List("characterContactsRead", "characterContactsWrite")
   val SESSION = "session"
 
@@ -26,16 +31,15 @@ object Webapp extends App {
 
   // index page
   get("/", (req: Request, resp: Response) => {
-    if (req.session.attribute[Types.Session](SESSION) != null) {
-      templates.html.base.apply("set.them.red", templates.html.landing.apply(), Some(req.session.attribute[Types.Session](SESSION)))
-    } else {
-      templates.html.base.apply("set.them.red", templates.html.landing.apply(), None)
+    req.getSession match {
+      case Some(s) => templates.html.base.apply("set.them.red", templates.html.main.apply(), Some(s))
+      case None => templates.html.base.apply("set.them.red", templates.html.landing.apply(), None)
     }
   })
   after("/", (req: Request, resp: Response) => {
-    val session = Option(req.session.attribute[Types.Session](SESSION))
+    val session = req.getSession
     session match {
-      case Some(s) => req.session.attribute(SESSION, s.copy(alerts = List()))
+      case Some(s) => req.clearAlerts
       case _ => ()
     }
   })
@@ -44,6 +48,52 @@ object Webapp extends App {
     req.session(true)
     resp.redirect(crest.redirect("", defaultCrestScopes))
   })
+  // post endpoints for doing basic things
+  post("/add/character", (req: Request, resp: Response) => {
+    req.getSession match {
+      case Some(s) =>
+        val name = req.queryParams("name")
+        val id = eveapi.eve.CharacterID(Seq(name)).sync().get.result.head.characterID.toLong
+        val result = Try { crest.contacts.createContact(s.characterID, s.accessToken, crest.contacts.createCharacterAddRequest(-10, id, name, true)).sync() }
+        result match {
+          case Success(r) => req.flash(Alerts.success, "Added %s to your watchlist".format(name))
+          case Failure(t) => req.flash(Alerts.danger, "Unable to add %s to your watchlist".format(name))
+        }
+        resp.redirect("/")
+      case None =>
+        resp.redirect("/")
+    }
+    ()
+  })
+  post("/add/characters", (req: Request, resp: Response) => {
+    req.getSession match {
+      case Some(s) =>
+        req.queryParams("names")
+          .split('\n')
+          .map(_.trim)
+          .grouped(250)
+          .map(s => eveapi.eve.CharacterID(s))
+          .foldRight(Seq.empty[CharacterID.Row]){ (n, a) =>
+            n.sync().get.result ++ a
+          }.map { row =>
+            val car = crest.contacts.createCharacterAddRequest(-10, row.characterID.toLong, row.name, true)
+            Try { crest.contacts.createContact(s.characterID, s.accessToken, car) }
+          }.map(s => s.map(_.sync()))
+          .groupBy(_.isSuccess)
+          .mapValues(_.size)
+          .foreach { kv =>
+            val (status, count) = kv
+            status match {
+              case true  => req.flash(Alerts.success, "Successfully added %d contacts to your watchlist.".format(count))
+              case false => req.flash(Alerts.danger, "Failed to add %d contacts to your watchlist.".format(count))
+            }
+          }
+        resp.redirect("/")
+      case None =>
+        resp.redirect("/")
+    }
+    ()
+  })
   // callback for when CCP auth sends them back
   get("/callback", (req: Request, resp: Response) => {
     val code = req.queryParams("code")
@@ -51,7 +101,7 @@ object Webapp extends App {
     val callbackresults = crest.callback(code).sync()
     val verify = crest.verify(callbackresults.access_token).sync()
     val session = new Types.Session(callbackresults.access_token, callbackresults.refresh_token.get, verify.characterName, verify.characterID, List(new Alert("success", "Thanks for logging in %s".format(verify.characterName))))
-    req.session.attribute(SESSION, session)
+    req.setSession(session)
     // go back to the index since we've just logged in
     resp.redirect("/")
   })
